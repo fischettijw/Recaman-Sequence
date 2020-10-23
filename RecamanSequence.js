098767b6acde8f0d828e0945a6b59284657eef0d
Joseph Fischetti <fischettijw@gmail.com>
2020-10-23 09:25:55 -0400

    created CLASS

--- RecamanSequence.js
+++ RecamanSequence.js
@@ -2,26 +2,34 @@ let numbers; // hasLanded      number already landed on
 let count; //  stepNumber   1,2,3,4,5, ...
 let sequence; // Recaman Sequence   0,1,3,6,2,7,13,20,12,21,11
 let index; // Recaman Sequence current position/index
+let arcs; // save arcs (semi-circles)
 
 function initialize() {
     numbers = [];
     count = 0;
     sequence = [];
     index = 0;
+    arcs = [];
 }
 
 function setup() {
     initialize();
     createCanvas(500, 500);
-    background('black');
+    // background('black');
 }
 
 function draw() {
+    background('black');
     step();
     debugOutput();
 
+    for (let ar of arcs) {
+        ar.show();
+    }
 
 }
+
+
 
 
 function step() {
@@ -32,13 +40,22 @@ function step() {
     numbers[next] = true;
     sequence.push(next);
 
-    let diameter = next - index;
-    let x = (next + index) / 2;
+    let a = new Arc(index, next, count % 2);
+    arcs.push(a);
 
-    stroke('white');
-    strokeWeight(1);
-    noFill();
-    ellipse(x, height / 2, diameter);
+    // let diameter = abs(next - index);
+    // let x = (next + index) / 2;
+
+    // stroke('white');
+    // strokeWeight(1);
+    // noFill();
+    // if (count % 2 == 0) {
+    //     arc(x, height / 2, diameter, diameter, PI, 0);
+    // } else {
+    //     arc(x, height / 2, diameter, diameter, 0, PI);
+    // }
+    // arc(x, height / 2, diameter, diameter, 0, PI);
+    // ellipse(x, height / 2, diameter);
 
 
     index = next;
@@ -48,12 +65,32 @@ function step() {
 
 
 
-
-
-
 function debugOutput() {
     if (frameCount >= 200) {
         noLoop();
         // print(sequence);
+    }
+}
+
+
+class Arc {
+    constructor(start, end, dir) {
+        this.start = start;
+        this.end = end;
+        this.dir = dir;
+    }
+
+    show() {
+        let diameter = abs(this.end - this.start);
+        let x = (this.end + this.start) / 2;
+
+        stroke('white');
+        strokeWeight(1);
+        noFill();
+        if (this.dir == 0) {
+            arc(x, height / 2, diameter, diameter, PI, 0);
+        } else {
+            arc(x, height / 2, diameter, diameter, 0, PI);
+        }
     }
 }
